299230a0d96503c3154d6fa86372e31da3d868b6
Brian Wu <wubrian1234@hotmail.com>
2019-01-17 23:59:56 +0000

    http basic auth complete

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_AUTH_NAME"], password: ENV["HTTP_AUTH_PASSWORD"]
+
   def index
     @categories = Category.order(:name).all
   end
